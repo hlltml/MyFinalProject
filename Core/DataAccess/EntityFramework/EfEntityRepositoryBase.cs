ce37638a4b71e9d58d3c8d7d1fd937aa2466dcd9
halil temel <haliltml91@gmail.com>
2021-02-14 18:52:05 +0300

    9. Gün Ders Sonu

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,64 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity,TContext> :IEntityRepository<TEntity>
+        where TEntity: class, IEntity, new()
+        where TContext: DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            //Aşağıda kullandığımız using'i araştırmak istersek: IDisposable pattern implementation of c#
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null
+                    ? context.Set<TEntity>().ToList() // Filtre null ise burası çalışır
+                    : context.Set<TEntity>().Where(filter).ToList(); // Filtre null değilse burası çalışır.
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -6,18 +6,19 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
+    //Veritabanı ile kendi classlarımızı ilişkilendirdiğimiz sınıf
     //Context : Db tabloları ile proje classlarını bağlamak
     public class NorthwindContext:DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            //Ters slash'ı (\) ters slash olarak algılaması için çft tırkaktan önce @ işareti kullanılır.
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
         }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
-
-
+        public DbSet<Order> Orders { get; set; }
     }
 }
